f3f9bf6e76183080697c7725934da47e52319cf2
Sumeet Gupta <sumeetgupta3690@gmail.com>
2024-09-06 14:56:52 +0530

    Task 35 - Responsive Navbar

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,21 +1,25 @@
-
-import React, {useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "../navbar/Navbar";
 
 const Home = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const user = useSelector((state) => state.auth.user);
-  const token = useSelector((state) => state.auth.token);
+ 
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    // console.log("Current Auth State:", { isAuthenticated, user, token });
-  }, [isAuthenticated, user, token]);
   return (
-    <div>Home </div>
-  )
-}
+    <div>
+      <div className="onlymobile">
+        <div className="topNavbar">
+          <i className="fas fa-cog"></i>
+          <img src={user.profilePic} alt="" />
+        </div>
+      </div>
+      <Navbar />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,15 +1,50 @@
-
-import React from 'react'
+import React from "react";
+import "./navbar.css";
+import "./mnavbar.css";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+
+  const user = useSelector((state) => state.auth.user);
+
+  // console.log(user);
+  
+
   return (
-    <div className='navbarBox'>
+    <div className="navBody">
 
-        <div className="flexIcons">
-            <i></i>
-        </div>
+      <div className="onlypc profileDetail">
+        <img src={user.profilePic} alt="" />
+      </div>
+
+     
+
+      <div className="compNav">
+        <i className="fas fa-home"></i>
+      </div>
+      <div className="compNav">
+        <i className="fas fa-search"></i>
+      </div>
+      <div className="compNav">
+        <i className="fas fa-plus-circle"></i>
+      </div>
+      <div className="compNav">
+        <i className="fas fa-edit"></i>
+      </div>
+      <div className="compNav">
+        <i className="fas fa-bell"></i>
+      </div>
+
+      <div className="onlypc downNav">
+      <div className="compNav">
+        <i className="fas fa-cog"></i>
+      </div>
+      <div className="compNav">
+        <i className="fas fa-power-off"></i>
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
